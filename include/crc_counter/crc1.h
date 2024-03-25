70b8ad736c83ad7d9a907226a95446d649bf8ed3
JustRictor <justrictor39@outlook.com>
2024-03-26 02:00:14 +0300

    add crc1 algorithm

--- include/crc_counter/crc1.h
+++ include/crc_counter/crc1.h
@@ -1,6 +1,10 @@
-#ifndef __CRC1_
-#define __CRC1_
+#ifndef __CRC1_H_
+#define __CRC1_H_
 
+#include "stdint.h"
 
+#define CRC1_POLY 0x1
 
-#endif
+uint8_t crc1(uint8_t* data, uint64_t len);
+
+#endif//__CRC1_H_
--- src/crc1.c
+++ src/crc1.c
@@ -1,1 +1,16 @@
 #include "crc_counter/crc1.h"
+
+uint8_t crc1(uint8_t* data, uint64_t len)
+{
+    uint8_t crc = 0x00;
+    while(len--)
+    {
+        crc ^= *data++;
+        for(uint8_t i = 0; i < 8; i++)
+        {
+            crc = crc & 0x01 ? ( crc << 1 ) ^ CRC1_POLY
+                             :   crc << 1 ;
+        }
+    }
+    return crc;
+}
