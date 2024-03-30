c88b4cdb82737a8ba6999b3c2f5e6e1e6c18c663
JustRictor <justrictor39@outlook.com>
2024-03-30 13:25:02 +0300

    add caclCrc16-32-64_table

--- include/crc_counter/tools/calc_crc_table.h
+++ include/crc_counter/tools/calc_crc_table.h
@@ -11,4 +11,22 @@ bool calc_crc8_table(uint8_t* crcTable,
                      uint8_t polyBitLen
         );
 
+bool calc_crc16_table(uint16_t* crcTable,
+                     uint16_t crcPolynom,
+                     bool refIn,
+                     uint8_t polyBitLen
+        );
+
+bool calc_crc32_table(uint32_t* crcTable,
+                     uint32_t crcPolynom,
+                     bool refIn,
+                     uint8_t polyBitLen
+        );
+
+bool calc_crc64_table(uint64_t* crcTable,
+                     uint64_t crcPolynom,
+                     bool refIn,
+                     uint8_t polyBitLen
+        );
+
 #endif//__CALC_CRC_TABLE_H_
--- src/tools/calc_crc_table.c
+++ src/tools/calc_crc_table.c
@@ -34,3 +34,108 @@ bool calc_crc8_table(uint8_t *crcTable,
     }
     return true;
 }
+
+bool calc_crc16_table(uint16_t *crcTable,
+                     uint16_t crcPolynom,
+                     bool refIn,
+                     uint8_t polyBitLen
+        )
+{
+    if(crcTable == NULL
+            || polyBitLen < 9
+            || polyBitLen > 16
+            )
+    {
+        return false;
+    }
+
+    for(uint16_t x = 0; x < 256; x++)
+    {
+        uint16_t crc = ( x << (polyBitLen - 8) );
+        for(uint8_t i = 0; i < 8; i++)
+        {
+            if (refIn == true)
+            {
+                crc = (crc & 1) ? ( crc >> 1 ) ^ crcPolynom
+                                :   crc >> 1 ;
+            }
+            else//refIn == false
+            {
+                crc = (crc & (1 << (polyBitLen - 1))) ? ( crc << 1 ) ^ crcPolynom
+                                                      :   crc << 1 ;
+            }
+        *crcTable++ = crc;
+        }
+    }
+    return true;
+}
+
+bool calc_crc32_table(uint32_t *crcTable,
+                     uint32_t crcPolynom,
+                     bool refIn,
+                     uint8_t polyBitLen
+        )
+{
+    if(crcTable == NULL
+            || polyBitLen < 17
+            || polyBitLen > 32
+            )
+    {
+        return false;
+    }
+
+    for(uint16_t x = 0; x < 256; x++)
+    {
+        uint32_t crc = ( x << (polyBitLen - 8) );
+        for(uint8_t i = 0; i < 8; i++)
+        {
+            if (refIn == true)
+            {
+                crc = (crc & 1) ? ( crc >> 1 ) ^ crcPolynom
+                                :   crc >> 1 ;
+            }
+            else//refIn == false
+            {
+                crc = (crc & (1 << (polyBitLen - 1))) ? ( crc << 1 ) ^ crcPolynom
+                                                      :   crc << 1 ;
+            }
+        *crcTable++ = crc;
+        }
+    }
+    return true;
+}
+
+bool calc_crc64_table(uint64_t *crcTable,
+                     uint64_t crcPolynom,
+                     bool refIn,
+                     uint8_t polyBitLen
+        )
+{
+    if(crcTable == NULL
+            || polyBitLen < 33
+            || polyBitLen > 64
+            )
+    {
+        return false;
+    }
+
+    for(uint16_t x = 0; x < 256; x++)
+    {
+        uint64_t crc = ( x << (polyBitLen - 8) );
+        for(uint8_t i = 0; i < 8; i++)
+        {
+            if (refIn == true)
+            {
+                crc = (crc & 1) ? ( crc >> 1 ) ^ crcPolynom
+                                :   crc >> 1 ;
+            }
+            else//refIn == false
+            {
+                crc = (crc & (1 << (polyBitLen - 1))) ? ( crc << 1 ) ^ crcPolynom
+                                                      :   crc << 1 ;
+            }
+        *crcTable++ = crc;
+        }
+    }
+    return true;
+}
